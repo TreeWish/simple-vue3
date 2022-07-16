c0c6b1f55ce80e4695d34f6f7df42510431e0ae4
TreeWish <1131093754@qq.com>
2022-07-16 13:44:13 +0800

    实现 isRef & unRef & proxyRefs 功能

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -6,6 +6,7 @@ class RefImpl {
   private _value: any
   public dep
   private _rawValue: any
+  public __v_isRef_: boolean = true
   constructor(value) {
     this._rawValue = value
     this._value = convert(value)
@@ -41,4 +42,25 @@ export function ref(value) {
   return new RefImpl(value)
 }
 
-export function isRef(value) {}
+export function isRef(ref) {
+  return !!ref.__v_isRef_
+}
+
+export function unRef(ref) {
+  return isRef(ref) ? ref.value : ref
+}
+
+export function proxyRefs(objectWithRefs) {
+  return new Proxy(objectWithRefs, {
+    get(target, key) {
+      return unRef(Reflect.get(target, key))
+    },
+    set(target, key, val) {
+      if (isRef(target[key]) && !isRef(val)) {
+        return target[key].value = val 
+      } else {
+        return Reflect.set(target, key, val)
+      }
+    },
+  })
+}
